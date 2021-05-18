89e576eda0c4fe554b266fc52d214ecd567ed55d
Endzhe Galieva <endzhe91@gmail.com>
2021-05-18 18:52:06 +0300

    edit and update contact action

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -58,13 +58,15 @@ const ContactState = props => {
     dispatch({ type: SET_CURRENT, payload: contact });
   };
 
-
   // Clear Current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
   };
 
   // Update Contact
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
 
   // Filter Contacts
 
@@ -78,7 +80,8 @@ const ContactState = props => {
         addContact,
         deleteContact,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        updateContact
       }}
     >
       { props.children }
